30b2af7af2b079c54635a36ec1fc33d259bb1d65
jzicc <123297148+jzicc@users.noreply.github.com>
2023-05-07 19:49:31 +0800

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,12 +14,15 @@ export class AppComponent {
 
 const numbersRange$ = range(1, 10);
 
+//1.
 numbersRange$
   .pipe(filter((num) => num % 2 === 0))
   .subscribe((value) => console.log('Even number: ' + value));
 
+//2.
 const odd = numbersRange$.pipe(filter((num) => num % 2 != 0));
 
 const subscribe$ = odd.subscribe((val) => console.log(`Odd number: ${val}`));
 
+//3.
 numbersRange$.subscribe((value) => console.log(value * 2 + ' => ' + value * 4));
